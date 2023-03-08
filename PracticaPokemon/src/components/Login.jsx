d73dcacf01656a2785b5ac8d978a5bdd20a50b09
jonfm09 <jonathan.fermanm@gmail.com>
2023-03-07 21:31:20 -0600

    Login.jsx

--- PracticaPokemon/src/components/Login.jsx
+++ PracticaPokemon/src/components/Login.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Auth0Provider, useAuth0 } from '@auth0/auth0-react'
 
 export default function Login() {
-    const {loginWithRedirect} = useAuth0
+    const {loginWithRedirect} = useAuth0();
   return (
-    <button onClick={() => loginWithRedirect()}> Iniciar Sesion </button>
+    <button className='btn btn-primary' onClick={() => loginWithRedirect()}> Iniciar Sesion </button>
   )
 }
